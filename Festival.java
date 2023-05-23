90701f20c793ff7c2b2ccb5d5d45ace93948c71c
Oscar <oscarfuersuarez27@gmail.com>
2023-05-23 13:01:54 +0200

    primer commit

---
+++ Festival.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Festival here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Festival
+{
+    // instance variables - replace the example below with your own
+    private String nombreFestival;
+    private int numeroCantantes;
+    private Entrada Entrada;
+    /**
+     * Constructor for objects of class Festival
+     */
+    public Festival(String nombreFestival, int numeroCantantes)
+    {
+        String nomFest = "";
+        int numCant = 0;
+    }
+}
