e2de585410c24f670fc263868869b29d179b1c77
Ponga Villacorta <dpongavillacorta@gmail.com>
2023-05-25 12:02:23 +0200

    Cuarto commit

--- Entrada.java
+++ Entrada.java
@@ -8,26 +8,15 @@
 public class Entrada
 {
     // instance variables - replace the example below with your own
-    private int x;
-
+    private int precio;
+    private int cantidad;
     /**
      * Constructor for objects of class Entrada
      */
     public Entrada()
     {
         // initialise instance variables
-        x = 0;
+
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
