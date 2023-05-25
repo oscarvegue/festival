26d1216a5c071ae9c317adc24dba5fe02647deb2
Ponga Villacorta <dpongavillacorta@gmail.com>
2023-05-25 11:04:10 +0200

    Segundo Commit

--- Festival.java
+++ Festival.java
@@ -10,13 +10,38 @@ public class Festival
     // instance variables - replace the example below with your own
     private String nombreFestival;
     private int numeroCantantes;
-    private Entrada Entrada;
+    private Entrada entrada;
     /**
      * Constructor for objects of class Festival
      */
-    public Festival(String nombreFestival, int numeroCantantes)
+    public Festival(String nombreFest, int numeroCant, Entrada ticket)
     {
-        String nomFest = "";
-        int numCant = 0;
+        nombreFestival = nombreFest;
+        numeroCantantes = numeroCant;
+        entrada = ticket;
+    }
+
+    public int getnumerocantantes(){
+        return numeroCantantes;
+    }
+
+    public String getnombrefestival(){
+        return nombreFestival;
+    }
+
+    public Entrada getTicket(){
+        return entrada;    
+    }
+
+    public void setNumerocantantes(int numerocantantes){
+        this.numeroCantantes = numerocantantes;
+    }
+
+    public void setNombrefestival(String nombrefestival){
+        this.nombreFestival = nombrefestival;
+    }
+
+    public void setEntrada(Entrada entrada){
+        this.entrada = entrada;
     }
 }
